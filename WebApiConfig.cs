a71221a1e17448f2f843fede1a544b5107736f24
krunalbhalani <krunalbhalani@gmail.com>
2016-05-25 22:36:37 +0100

    Add files via upload

---
+++ WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace PrimeNumber
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+            config.Routes.MapHttpRoute(
+    name: "DefaultApiPrimeNumber",
+    routeTemplate: "api/{controller}/{number}",
+    defaults: new { number = RouteParameter.Optional }
+    );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+        
+       }
+    }
+}
